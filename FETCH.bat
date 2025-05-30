661cec325e37f41d9636810b7cbc8d2e9879207b
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2025-05-30 17:39:11 +0300

    Update FETCH.bat

--- FETCH.bat
+++ FETCH.bat
@@ -1,3 +1,5 @@
+:memreductinstall
 cd C:\Users\WDAGUtilityAccount\Desktop
 curl -L -o memreduct.exe https://memreduct.org/files/memreduct-3.5.2-setup.exe
 memreduct.exe /silent
+if EXIST "C:\Program Files\Mem Reduct\memreduct.exe" (echo File exists) else (goto memreductinstall)
